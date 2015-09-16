47485b30238f03051d33b9336ce31070895c87ae
Mathias Bredholt <mathiasbredholt@gmail.com>
2015-09-16 11:30:23 +0200

    added conf

--- mathnotes.py
+++ mathnotes.py
@@ -15,6 +15,6 @@ def run():
         calc(prompt)
 
 def calc(input):
-    cmd = "java -cp {} frink.parser.Frink -e '{}'".format(frinkPath, input);
+    cmd = "java -cp '{}' frink.parser.Frink -e '{}'".format(frinkPath, input);
     proc = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=STDOUT, universal_newlines=True, shell=True)
     print(proc.stdout.readline())
