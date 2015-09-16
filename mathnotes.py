5e61cb6c53b299f248309f4b82d9901cae08f0de
Mathias Bredholt <mathiasbredholt@gmail.com>
2015-09-16 11:24:06 +0200

    added conf

--- mathnotes.py
+++ mathnotes.py
@@ -2,7 +2,14 @@ from subprocess import Popen, PIPE, STDOUT
 
 frinkPath = "/Applications/Frink/frink.jar"
 
+def conf():
+    f = open('mathnotes.conf', 'w')
+    f.write(frinkPath)
+
 def run():
+    f = open('mathnotes.conf', 'r')
+    frinkPath = f.read()
+    print(frinkPath)
     while True:
         prompt = input("> ")
         calc(prompt)
